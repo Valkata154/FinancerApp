75deb824e751a426b0d791f24f74bca6e199c527
Valeri Vladimirov <40399682@live.napier.ac.uk>
2020-12-01 13:49:52 +0200

    Created a Watchlist menu that updates the price based on the actual market.

---
+++ Financer/Ticker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YahooFinanceApi;
+
+namespace Financer
+{
+    public class Ticker
+    {
+        private string name;
+        private double currPrice;
+        private double openPrice;
+        private string volume;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double CurrPrice
+        {
+            get { return currPrice; }
+            set { currPrice = value; }
+        }
+
+        public double OpenPrice
+        {
+            get { return openPrice; }
+            set { openPrice = value; }
+        }
+
+        public string Volume
+        {
+            get { return volume; }
+            set { volume = value; }
+        }
+    }
+}
